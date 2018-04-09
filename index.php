623c1259db2cd14ef21e3e81a7c8e0544015efc8
vaddi <vaddi@mvattersen.de>
2018-04-09 19:39:50 +0200

    Add Matomo

--- index.php
+++ index.php
@@ -163,5 +163,21 @@ $entfernungen = $luncher->getEntfernungen();
 	
 </div>
 
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//mgmt.marienfeldt.net/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '6']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+
 </body>
 </html>
